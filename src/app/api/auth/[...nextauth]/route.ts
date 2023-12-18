b838bc9dc22241a6753f0647958b98641df89550
Santi Singha <santisingha191@gmail.com>
2023-12-18 21:16:14 +0530

    import AuthOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,7 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { AuthOptions  } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { cookies } from 'next/headers'
-import { graphqlClient } from "../../../clients/api";
-import { verifyUserGoogleTokenQuery } from "../../../graphql/query/user";
 
-async function customSignIn(idToken: string) {
-  try {
-    const googleToken = idToken;
-    if (!googleToken) {
-      throw new Error("Token is not present");
-    } else {
-      const { verifyGoogleToken } = await graphqlClient.request(
-        verifyUserGoogleTokenQuery,
-        { token: googleToken }
-      );
-      if (!verifyGoogleToken) {
-        throw new Error("JWT token is not generated");
-      }
-      cookies().set({
-        name: 'OWN_TWITTER_TOKEN',
-        value: verifyGoogleToken,
-        maxAge: 30*24*60*60,
-        httpOnly: false,
-        path: '/',
-      });
-      return verifyGoogleToken;
-    }
-  } catch (error) {
-    console.error("Error in customSignInCallback: ", error);
-  }
-}
-
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions  = {
   session: {
     strategy: "jwt",
   },
